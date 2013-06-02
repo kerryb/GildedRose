c72bacb6cc642fbce16b769de951dcf66fe3abf8
Kerry Buckley <kerryjbuckley@gmail.com>
2013-06-02 12:34:45 +0100

    Extract methods to check item type

--- src/main/java/GildedRose.java
+++ src/main/java/GildedRose.java
@@ -26,32 +26,29 @@ public class GildedRose {
   }
 
   private static void updateItemSellIn(final Item item) {
-    if (!"Sulfuras, Hand of Ragnaros".equals(item.getName())) {
+    if (!isLegendary(item)) {
       item.setSellIn(item.getSellIn() - 1);
     }
   }
 
   private static void updateItemQuality(final Item item) {
-    if ((!"Aged Brie".equals(item.getName()))
-        && !"Backstage passes to a TAFKAL80ETC concert".equals(item
-            .getName())) {
+    if ((!improvesWithAge(item)) && !isBackstagePass(item)) {
       if (item.getQuality() > 0) {
-        if (!"Sulfuras, Hand of Ragnaros".equals(item.getName())) {
+        if (!isLegendary(item)) {
           item.setQuality(item.getQuality() - 1);
         }
       }
     } else {
       if (item.getQuality() < 50) {
         item.setQuality(item.getQuality() + 1);
-  
-        if ("Backstage passes to a TAFKAL80ETC concert".equals(item
-            .getName())) {
+
+        if (isBackstagePass(item)) {
           if (item.getSellIn() < 11) {
             if (item.getQuality() < 50) {
               item.setQuality(item.getQuality() + 1);
             }
           }
-  
+
           if (item.getSellIn() < 6) {
             if (item.getQuality() < 50) {
               item.setQuality(item.getQuality() + 1);
@@ -62,17 +59,15 @@ public class GildedRose {
     }
 
     if (item.getSellIn() < 0) {
-      if (!"Aged Brie".equals(item.getName())) {
-        if (!"Backstage passes to a TAFKAL80ETC concert".equals(item
-            .getName())) {
+      if (!improvesWithAge(item)) {
+        if (!isBackstagePass(item)) {
           if (item.getQuality() > 0) {
-            if (!"Sulfuras, Hand of Ragnaros".equals(item.getName())) {
+            if (!isLegendary(item)) {
               item.setQuality(item.getQuality() - 1);
             }
           }
         } else {
-          item.setQuality(
-              item.getQuality() - item.getQuality());
+          item.setQuality(item.getQuality() - item.getQuality());
         }
       } else {
         if (item.getQuality() < 50) {
@@ -80,5 +75,17 @@ public class GildedRose {
         }
       }
     }
+  }
+
+  private static boolean isBackstagePass(final Item item) {
+    return "Backstage passes to a TAFKAL80ETC concert".equals(item.getName());
+  }
+
+  private static boolean isLegendary(final Item item) {
+    return "Sulfuras, Hand of Ragnaros".equals(item.getName());
+  }
+
+  private static boolean improvesWithAge(final Item item) {
+    return "Aged Brie".equals(item.getName());
   }
 }
